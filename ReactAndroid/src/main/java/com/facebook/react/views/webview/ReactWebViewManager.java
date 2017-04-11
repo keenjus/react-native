e8f728e3a6ae3b13d04de5a6f0a5eba4bda5c1d4
Etheryte <eth@mango.local>
2017-04-11 17:50:35 +0300

    I can't type for shit today

--- ReactAndroid/src/main/java/com/facebook/react/views/webview/ReactWebViewManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/webview/ReactWebViewManager.java
@@ -339,7 +339,7 @@ public class ReactWebViewManager extends SimpleViewManager<WebView> {
     webView.getSettings().setBuiltInZoomControls(true);
     webView.getSettings().setDisplayZoomControls(false);
     webView.getSettings().setDomStorageEnabled(true);
-		webView.getSettings().setMixedContent(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+		webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
 		webView.getSettings().setAllowFileAccessFromFileURLs(true);
 		webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
 
